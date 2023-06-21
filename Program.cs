94d76f7b15ae1319889c3e66d60573465a252bf7
bodyadev <topchii.rxz@gmail.com>
2023-06-21 15:26:01 +0200

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,46 @@
+using Domain;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Set up DB SQLite connection
+builder.Services.AddDbContext<DatabaseContext>(opt => {
+    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+// ADD CORS POLICY
+builder.Services.AddCors(options => {
+        
+    options.AddDefaultPolicy(policy => {
+        policy.AllowAnyMethod()
+            .AllowAnyOrigin()
+            .AllowAnyHeader();
+    });
+});
+
+var app = builder.Build();
+
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+// USE CORS POLICY
+app.UseCors();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
